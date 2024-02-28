c7262c53257cd1714739b3f82b080937c4d7e6f0
ni vi <fokin-klock@mail.ru>
2024-02-28 15:59:11 +0400

    end

---
+++ encryption/ReplaceOneLetter.py
@@ -0,0 +1,52 @@
+
+class ReplaceOneLetter:
+
+
+    def __init__(self, originalDict: str, replaceDict: str):
+        
+        if len(originalDict) != len(replaceDict) or self.__checkDuplicatesInStr(originalDict) or self.__checkDuplicatesInStr(replaceDict):
+            raise "fail"
+
+        self._originalDict = dict(zip(originalDict, replaceDict))
+        self._replaceDict = dict(zip(replaceDict, originalDict))
+    
+    def encrypt(self, string: str) -> str:
+
+        if not self.__checkCharacters(string, self._originalDict):
+            raise "fail"
+        
+        encryptik = ""
+
+        for i in range(len(string)):
+            encryptik += self._originalDict[string[i]]
+
+        return encryptik.rever
+
+    def translate(self, string: str) -> str:
+
+        if not self.__checkCharacters(string, self._replaceDict):
+            raise "fail"
+        
+        encryptik = ""
+
+        for i in range(len(string)):
+            encryptik += self._replaceDict[string[i]]
+
+        return encryptik
+
+    def __checkDuplicatesInStr(self, strka: str) -> bool:
+        
+        return len(set(strka)) != len(strka)
+    
+
+    def __checkCharacters(self, forTranslate: str, dictionary: dict) -> bool:
+        
+        for i in forTranslate:
+            
+            if i not in dictionary.values():
+                return False
+            
+        return True
+        
+
+        
